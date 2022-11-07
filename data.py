ba8fa890dd1fe676d50be241f2806fa1fe51e1fb
chandu <chanduattina@gmail.com>
2022-11-07 17:34:04 +0530

    added new data

--- data.py
+++ data.py
@@ -3,6 +3,7 @@ from pydantic import BaseModel
 geetha_record = {"details": "Geetha", "id": 1, "yob": 2002}
 chandu_record = {"details": "Chandu", "id": 2, "yob": 2002}
 ravin_record = {"name": "Ravin", "id": 3, "yob": 1982}
+sudharsan_record = {"name": "sudar", "id": 4, "yob": 2002}
 
 employees = [geetha_record, chandu_record, ravin_record]
 
