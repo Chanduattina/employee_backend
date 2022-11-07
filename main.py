2f6fcdfe215a010db6378b619ea1b652084ba09d
chandu <chanduattina@gmail.com>
2022-11-07 17:29:21 +0530

    reformatted file checkin

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 from fastapi import FastAPI
 from data import employees, Item
-from utils import fast_find_employee_by_id,find_employee_by_id
+from utils import fast_find_employee_by_id, find_employee_by_id
 
 app = FastAPI()
-
 
 
 @app.get("/all_employees/")
@@ -29,10 +28,8 @@ def get_employee_rec_fast(id: int = None):
         return None
     return fast_find_employee_by_id(id)
 
+
 @app.post("/update_employee_data/")
-def update_employee_data(data:Item):
+def update_employee_data(data: Item):
     employees.append(data.dict())
-    return {"updated list":employees}
-
-
-
+    return {"updated list": employees}
